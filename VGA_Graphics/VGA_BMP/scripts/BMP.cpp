e3a85a59f48f331e5ce60e819e072d0cc6fcdaac
Noah Breedy <breedyn@sunypoly.edu>
2024-02-05 19:43:26 -0500

    Fixed converter

--- VGA_Graphics/VGA_BMP/scripts/BMP.cpp
+++ VGA_Graphics/VGA_BMP/scripts/BMP.cpp
@@ -68,10 +68,16 @@ int main(int argc, char *argv[]){
     }
 
     std::vector<enum colors> bmp_pixels;
-    for(int i = 0; i < (width*height)*4; i+=4){
+    int count = 0;
+    std::cout << width << " " << height << std::endl;
+    for(int i = 0; i < (width*height); i+=1){
         uint8_t r = image_data[i];
         uint8_t g = image_data[i+1];
         uint8_t b = image_data[i+2];
+        if(0 <= count && count < 64){
+            std::cout << (int) r << " " << (int) g << " " << (int) b << " -> " << rgb_to_color_enum(r, g, b) << std::endl;
+        }
+        count ++;
         bmp_pixels.push_back(rgb_to_color_enum(r, g, b));
     }
 
@@ -81,11 +87,16 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Error opening file.\n");
         return 1;  
     }
-    fprintf(out_file, "%d\n", width);
-    fprintf(out_file, "%d\n", height);
+    fprintf(out_file,"unsigned char vga_data[] = {\n\t");
+    fprintf(out_file, "0x%x, 0x%x, ", width, height);
     for (int i = 0; i < bmp_pixels.size(); i++) {
-        fprintf(out_file, "%d\n", bmp_pixels[i]);
+        fprintf(out_file, "0x%x, ", bmp_pixels[i]);
+        if(i % 12 == 0 && i != 0){
+            fprintf(out_file, "\n\t", bmp_pixels[i]);
+        }
     }
+    fprintf(out_file,"\n}");
+
 
     fclose(out_file);
     stbi_image_free(image_data);
